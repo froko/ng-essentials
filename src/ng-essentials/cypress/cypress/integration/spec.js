a82bce2e55913d5b429102f6f71678b3fd05e968
Patrick Ineichen <github@frokonet.ch>
2018-09-14 19:59:40 +0300

    Integrated cypress's spec.js into git

---
+++ src/ng-essentials/cypress/cypress/integration/spec.js
@@ -0,0 +1,11 @@
+describe('Hello Angular', () => {
+  beforeEach(() => { cy.visit('http://localhost:4200'); });
+
+  it('should display welcome message', () => {
+    cy.get('app-root h1').should('contain', 'Welcome');
+  });
+
+  it('has 3 links', () => {
+    cy.get('app-root li a').should('have.length', 3);
+  });
+});
