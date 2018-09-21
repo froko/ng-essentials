4ce96c82a7f3e26160d9c9c30c995d89a91b8155
Patrick Ineichen <github@frokonet.ch>
2018-09-21 17:43:59 +0200

    Added unit tests

---
+++ src/library/index_spec.ts
@@ -0,0 +1,213 @@
+import { dasherize } from '@angular-devkit/core/src/utils/strings';
+import { Tree, VirtualTree } from '@angular-devkit/schematics';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+
+import * as path from 'path';
+
+import { PACKAGE_JSON, ANGULAR_JSON } from '../constants';
+import { library } from '../versions';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('library', () => {
+  const libraryName = 'myLib';
+  const dasherizedLibraryName = dasherize(libraryName);
+
+  let appTree: Tree;
+
+  beforeEach(() => {
+    appTree = new VirtualTree();
+    appTree = createPackageJson(new UnitTestTree(appTree));
+  });
+
+  describe('when creating a new library', () => {
+    let tree: UnitTestTree;
+
+    beforeEach(() => {
+      appTree = createAngularJsonWithoutJestOption(new UnitTestTree(appTree));
+
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+      tree = runner.runSchematic('library', { name: libraryName }, appTree);
+    });
+
+    it('adds files from the original @angular/schematics command', () => {
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/karma.conf.js`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/ng-package.json`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/package.json`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/tsconfig.lib.json`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/tsconfig.spec.json`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/tslint.json`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/src/public_api.ts`);
+      expect(tree.files).toContain(`/projects/${dasherizedLibraryName}/src/test.ts`);
+    });
+
+    it('removes automatic update symbols from package.json', () => {
+      expect(tree.readContent(PACKAGE_JSON)).not.toContain('^');
+      expect(tree.readContent(PACKAGE_JSON)).not.toContain('~');
+      expect(tree.readContent(PACKAGE_JSON)).not.toContain('>=');
+    });
+
+    it('updates angular packages in package.json', () => {
+      expect(tree.readContent(PACKAGE_JSON)).toContain(
+        `"@angular-devkit/build-ng-packagr": "${library.buildNgPackagrVersion}"`
+      );
+      expect(tree.readContent(PACKAGE_JSON)).toContain(`"ng-packagr": "${library.ngPackagrVersion}"`);
+      expect(tree.readContent(PACKAGE_JSON)).toContain(`"tsickle": "${library.tsickleVersion}"`);
+      expect(tree.readContent(PACKAGE_JSON)).toContain(`"tslib": "${library.tslibVersion}"`);
+    });
+
+    it('adds "no-implicit-dependencies" option to TsLint config of library', () => {
+      expect(tree.readContent(`/projects/${dasherizedLibraryName}/tslint.json`)).toContain(
+        '"no-implicit-dependencies": false'
+      );
+    });
+  });
+
+  describe('when creating a new library with jest option', () => {
+    let tree: UnitTestTree;
+
+    beforeEach(() => {
+      appTree = createAngularJsonWithJestOption(new UnitTestTree(appTree));
+
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+      tree = runner.runSchematic('library', { name: libraryName }, appTree);
+    });
+
+    it('removes karma config of library', () => {
+      expect(tree.files).not.toContain(`/projects/${dasherizedLibraryName}/karma.conf.js`);
+    });
+
+    it('removes test typescript file of library', () => {
+      expect(tree.files).not.toContain(`/projects/${dasherizedLibraryName}/src/test.ts`);
+    });
+
+    it('updates application typescript config file in src folder', () => {
+      expect(tree.readContent(`/projects/${dasherizedLibraryName}/tsconfig.lib.json`)).not.toContain('test.ts');
+    });
+
+    it('updates spec typescript config file in src folder', () => {
+      expect(tree.readContent(`/projects/${dasherizedLibraryName}/tsconfig.spec.json`)).not.toContain('files');
+      expect(tree.readContent(`/projects/${dasherizedLibraryName}/tsconfig.spec.json`)).not.toContain('jasmine');
+
+      expect(tree.readContent(`/projects/${dasherizedLibraryName}/tsconfig.spec.json`)).toContain('jest');
+      expect(tree.readContent(`/projects/${dasherizedLibraryName}/tsconfig.spec.json`)).toContain('commonjs');
+    });
+
+    it('switches to jest builder in angular.json', () => {
+      expect(tree.readContent(ANGULAR_JSON)).toContain('@angular-builders/jest:run');
+    });
+  });
+});
+
+function createAngularJsonWithoutJestOption(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    ANGULAR_JSON,
+    `{
+        "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+        "newProjectRoot": "projects",
+        "projects": {
+          "froko-app": {
+            "schematics": {
+              "@froko/ng-essentials": {
+                "jest": false,
+                "cypress": false,
+                "testcafe": false
+              }
+            },
+            "architect": {
+              "test": {
+                "builder": "@angular-devkit/build-angular:dev-server"
+              }
+            }
+          },
+          "froko-app-e2e": {}
+        },
+        "defaultProject": "froko-app"
+      }`
+  );
+
+  return tree;
+}
+
+function createAngularJsonWithJestOption(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    ANGULAR_JSON,
+    `{
+          "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+          "newProjectRoot": "projects",
+          "projects": {
+            "froko-app": {
+              "schematics": {
+                "@froko/ng-essentials": {
+                  "jest": true,
+                  "cypress": false,
+                  "testcafe": false
+                }
+              },
+              "architect": {
+                "test": {
+                  "builder": "@angular-devkit/build-angular:dev-server"
+                }
+              }
+            },
+            "froko-app-e2e": {}
+          },
+          "defaultProject": "froko-app"
+        }`
+  );
+
+  return tree;
+}
+
+function createPackageJson(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    PACKAGE_JSON,
+    `{
+        "scripts": {
+          "ng": "ng",
+          "start": "ng serve",
+          "build": "ng build",
+          "test": "ng test",
+          "lint": "ng lint",
+          "e2e": "ng e2e"
+        },
+        "dependencies": {
+          "@angular/animations": "^6.1.0",
+          "@angular/common": "^6.1.0",
+          "@angular/compiler": "^6.1.0",
+          "@angular/core": "^6.1.0",
+          "@angular/forms": "^6.1.0",
+          "@angular/http": "^6.1.0",
+          "@angular/platform-browser": "^6.1.0",
+          "@angular/platform-browser-dynamic": "^6.1.0",
+          "@angular/router": "^6.1.0",
+          "core-js": "^2.5.4",
+          "rxjs": "~6.2.0",
+          "zone.js": ">=0.8.26"
+        },
+        "devDependencies": {
+          "@angular-devkit/build-angular": "~0.8.0",
+          "@angular/cli": "~6.2.2",
+          "@angular/compiler-cli": "^6.1.0",
+          "@angular/language-service": "^6.1.0",
+          "@types/jasmine": "~2.8.8",
+          "@types/jasminewd2": "~2.0.3",
+          "@types/node": "~8.9.4",
+          "codelyzer": "~4.3.0",
+          "jasmine-core": "~2.99.1",
+          "jasmine-spec-reporter": "~4.2.1",
+          "karma": "~3.0.0",
+          "karma-chrome-launcher": "~2.2.0",
+          "karma-coverage-istanbul-reporter": "~2.0.1",
+          "karma-jasmine": "~1.1.2",
+          "karma-jasmine-html-reporter": "^0.2.2",
+          "protractor": "~5.4.0",
+          "ts-node": "~7.0.0",
+          "tslint": "~5.11.0",
+          "typescript": "~2.9.2"
+        }
+    }`
+  );
+
+  return tree;
+}
--- src/ng-essentials/index_spec.ts
+++ src/ng-essentials/index_spec.ts
@@ -1,7 +1,11 @@
 import { Tree, VirtualTree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import { createAppModule } from '@schematics/angular/utility/test/create-app-module';
+
 import * as path from 'path';
+
+import { ANGULAR_JSON, PACKAGE_JSON, TSLINT_JSON, TSCONFIGAPP_JSON } from '../constants';
+import { essentials, jest, cypress, testcafe } from '../versions';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
@@ -10,25 +14,422 @@ describe('ng-essentials', () => {
 
   beforeEach(() => {
     appTree = new VirtualTree();
+    appTree = createPackageJson(new UnitTestTree(appTree));
     appTree = createAppModule(new UnitTestTree(appTree));
+    appTree = createGlobalTsLintJson(new UnitTestTree(appTree));
     appTree = createDevelopmentEnvironmentFile(new UnitTestTree(appTree));
     appTree = createProductionEnvironmentFile(new UnitTestTree(appTree));
+    appTree = createKarmaConfig(new UnitTestTree(appTree));
+    appTree = createTestTypescriptFile(new UnitTestTree(appTree));
+    appTree = createTsConfigAppInSrcDirectory(new UnitTestTree(appTree));
+    appTree = createTsConfigSpecInSrcDirectory(new UnitTestTree(appTree));
   });
 
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('ng-add', {}, appTree);
+  describe('when running for the first time', () => {
+    beforeEach(() => {
+      appTree = createAngularJsonForFirstRun(new UnitTestTree(appTree));
+    });
 
-    expect(tree.files).toEqual([
-      '/src/app/app.module.ts',
-      '/src/environments/environment.ts',
-      '/src/environments/environment.prod.ts',
-      '/.npmrc',
-      '/.prettierrc',
-      '/.vscode/launch.json'
-    ]);
+    describe('without options', () => {
+      let tree: UnitTestTree;
+
+      beforeEach(() => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        tree = runner.runSchematic('ng-add', {}, appTree);
+      });
+
+      it('adds default collection to angular.json', () => {
+        expect(tree.readContent(ANGULAR_JSON)).toContain('"defaultCollection": "@froko/ng-essentials"');
+      });
+
+      it('adds ng-essentials options to angular.json', () => {
+        expect(tree.readContent(ANGULAR_JSON)).toContain('"jest": false');
+        expect(tree.readContent(ANGULAR_JSON)).toContain('"cypress": false');
+        expect(tree.readContent(ANGULAR_JSON)).toContain('"testcafe": false');
+      });
+
+      it('removes e2e test node from angular.json', () => {
+        expect(tree.readContent(ANGULAR_JSON)).not.toContain('froko-app-e2e');
+      });
+
+      it('removes protractor packages from angular.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('protractor');
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('@types/jasminewd2');
+      });
+
+      it('removes e2e script from package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('e2e');
+      });
+
+      it('removes automatic update symbols from package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('^');
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('~');
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('>=');
+      });
+
+      it('updates angular packages in package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/animations": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/common": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/compiler": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/core": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/forms": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/http": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/platform-browser": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(
+          `"@angular/platform-browser-dynamic": "${essentials.angularVersion}"`
+        );
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/router": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"core-js": "${essentials.coreJsVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"rxjs": "${essentials.rxjsVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(
+          `"@angular-devkit/build-angular": "${essentials.buildAngularVersion}"`
+        );
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/cli": "${essentials.cliVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/compiler-cli": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular/language-service": "${essentials.angularVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@types/node": "${essentials.nodeVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"codelyzer": "${essentials.codelizerVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"ts-node": "${essentials.tsNodeVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"tslint": "${essentials.tsLintVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"typescript": "${essentials.typescriptVersion}"`);
+      });
+
+      it('adds additional packages in package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"husky": "${essentials.huskyVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"npm-run-all": "${essentials.npmRunAllVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"prettier": "${essentials.prettierVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"pretty-quick": "${essentials.prettyQuickVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(
+          `"tslint-config-prettier": "${essentials.tsLintConfigPrettierVersion}"`
+        );
+      });
+
+      it('adds additional scripts in package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain('format');
+        expect(tree.readContent(PACKAGE_JSON)).toContain('format:check');
+        expect(tree.readContent(PACKAGE_JSON)).toContain('format:fix');
+        expect(tree.readContent(PACKAGE_JSON)).toContain('precommit');
+      });
+
+      it('updates global tslint.json', () => {
+        expect(tree.readContent(TSLINT_JSON)).toContain('"tslint:latest"');
+        expect(tree.readContent(TSLINT_JSON)).toContain('"tslint-config-prettier"');
+
+        expect(tree.readContent(TSLINT_JSON)).not.toContain('eofline');
+        expect(tree.readContent(TSLINT_JSON)).not.toContain('whitespace');
+
+        expect(tree.readContent(TSLINT_JSON)).toContain('"jsdoc-format": false');
+        expect(tree.readContent(TSLINT_JSON)).toContain('"no-implicit-dependencies"');
+        expect(tree.readContent(TSLINT_JSON)).toContain('"no-submodule-imports": false');
+      });
+
+      it('updates development environment file', () => {
+        expect(tree.readContent('src/environments/environment.ts')).toContain(
+          "{ provide: 'environment', useValue: 'Development' },"
+        );
+        expect(tree.readContent('src/environments/environment.ts')).toContain(
+          'export const ENV_PROVIDERS = providers;'
+        );
+      });
+
+      it('updates production environment file', () => {
+        expect(tree.readContent('src/environments/environment.prod.ts')).toContain(
+          "{ provide: 'environment', useValue: 'Production' },"
+        );
+        expect(tree.readContent('src/environments/environment.prod.ts')).toContain(
+          'export const ENV_PROVIDERS = providers;'
+        );
+      });
+
+      it('adds ENV_PROVIDERS to app module', () => {
+        expect(tree.readContent('src/app/app.module.ts')).toContain(
+          "import { ENV_PROVIDERS } from '../environments/environment';"
+        );
+        expect(tree.readContent('src/app/app.module.ts')).toContain('providers: [ENV_PROVIDERS]');
+      });
+
+      it('adds essentials files', () => {
+        expect(tree.files).toContain('/.npmrc');
+        expect(tree.files).toContain('/.prettierrc');
+        expect(tree.files).toContain('/.vscode/launch.json');
+      });
+
+      it('updates jasmine packages in package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain('"jasmine-core": "3.2.1"');
+      });
+    });
+
+    describe('with jest option', () => {
+      let tree: UnitTestTree;
+
+      beforeEach(() => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        tree = runner.runSchematic('ng-add', { jest: true }, appTree);
+      });
+
+      it('removes karma config file', () => {
+        expect(tree.files).not.toContain('/src/karma.conf.js');
+      });
+
+      it('removes test typescript file', () => {
+        expect(tree.files).not.toContain('/src/test.ts');
+      });
+
+      it('removes jasmine and karma packages from packages.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('jasmine');
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('karma');
+      });
+
+      it('adds jest packages to packages.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@angular-builders/jest": "${jest.jestBuilderVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"@types/jest": "${jest.jestTypeVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"babel-core": "${jest.babelCoreVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"babel-jest": "${jest.babelJestVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"jest": "${jest.jestVersion}"`);
+      });
+
+      it('switches to jest builder in angular.json', () => {
+        expect(tree.readContent(ANGULAR_JSON)).toContain('@angular-builders/jest:run');
+      });
+
+      it('updates application typescript config file in src folder', () => {
+        expect(tree.readContent(TSCONFIGAPP_JSON)).not.toContain('test.ts');
+      });
+
+      it('updates spec typescript config file in src folder', () => {
+        expect(tree.readContent('/src/tsconfig.spec.json')).not.toContain('files');
+        expect(tree.readContent('/src/tsconfig.spec.json')).not.toContain('jasmine');
+
+        expect(tree.readContent('/src/tsconfig.spec.json')).toContain('jest');
+        expect(tree.readContent('/src/tsconfig.spec.json')).toContain('commonjs');
+      });
+    });
+
+    describe('with cypress option', () => {
+      let tree: UnitTestTree;
+
+      beforeEach(() => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        tree = runner.runSchematic('ng-add', { cypress: true }, appTree);
+      });
+
+      it('adds cypress packages to packages.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"concurrently": "${cypress.concurrentlyVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"cypress": "${cypress.cypressVersion}"`);
+      });
+
+      it('adds cypress files', () => {
+        expect(tree.files).toContain('/cypress/fixtures/example.json');
+        expect(tree.files).toContain('/cypress/integration/spec.js');
+        expect(tree.files).toContain('/cypress/plugins/index.js');
+        expect(tree.files).toContain('/cypress/support/commands.js');
+        expect(tree.files).toContain('/cypress/support/index.js');
+      });
+    });
+
+    describe('with testcafe option', () => {
+      let tree: UnitTestTree;
+
+      beforeEach(() => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        tree = runner.runSchematic('ng-add', { testcafe: true }, appTree);
+      });
+
+      it('adds testcafe packages to packages.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"testcafe": "${testcafe.testcafeVersion}"`);
+        expect(tree.readContent(PACKAGE_JSON)).toContain(
+          `"testcafe-angular-selectors": "${testcafe.angularSelectorsVersion}"`
+        );
+        expect(tree.readContent(PACKAGE_JSON)).toContain(`"testcafe-live": "${testcafe.liveVersion}"`);
+      });
+
+      it('adds testcafe files', () => {
+        expect(tree.files).toContain('/testcafe/index.e2e-spec.js');
+      });
+    });
+  });
+
+  describe('when running for a subsequent time', () => {
+    beforeEach(() => {
+      appTree = createAngularJsonForSubsequentRun(new UnitTestTree(appTree));
+    });
+
+    describe('with all options', () => {
+      let tree: UnitTestTree;
+
+      beforeEach(() => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        tree = runner.runSchematic('ng-add', { jest: true, cypress: true, tesetcafe: true }, appTree);
+      });
+
+      it('does not add default collection to angular.json', () => {
+        expect(tree.readContent(ANGULAR_JSON)).not.toContain('"defaultCollection": "@froko/ng-essentials"');
+      });
+
+      it('does not add essentials files', () => {
+        expect(tree.files).not.toContain('/.npmrc');
+        expect(tree.files).not.toContain('/.prettierrc');
+        expect(tree.files).not.toContain('/.vscode/launch.json');
+      });
+
+      it('does not update jasmine packages in package.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).toContain('"jasmine-core": "~2.99.1"');
+      });
+
+      it('does not delete karma config file', () => {
+        expect(tree.files).toContain('/src/karma.conf.js');
+      });
+
+      it('does not switch to jest builder in angular.json', () => {
+        expect(tree.readContent(ANGULAR_JSON)).toContain('@angular-devkit/build-angular:dev-server');
+      });
+
+      it('does not add cypress packages to packages.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('concurrently');
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('cypress');
+      });
+
+      it('does not add cypress files', () => {
+        expect(tree.files).not.toContain('/cypress/fixtures/example.json');
+        expect(tree.files).not.toContain('/cypress/integration/spec.js');
+        expect(tree.files).not.toContain('/cypress/plugins/index.js');
+        expect(tree.files).not.toContain('/cypress/support/commands.js');
+        expect(tree.files).not.toContain('/cypress/support/index.js');
+      });
+
+      it('does not add testcafe packages to packages.json', () => {
+        expect(tree.readContent(PACKAGE_JSON)).not.toContain('testcafe');
+      });
+
+      it('does not add testcafe files', () => {
+        expect(tree.files).not.toContain('/testcafe/index.e2e-spec.js');
+      });
+    });
   });
 });
+
+function createAngularJsonForFirstRun(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    ANGULAR_JSON,
+    `{
+      "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+      "projects": {
+        "froko-app": {
+          "schematics": {},
+          "architect": {
+            "test": {
+              "builder": "@angular-devkit/build-angular:dev-server"
+            }
+          }
+        },
+        "froko-app-e2e": {}
+      },
+      "defaultProject": "froko-app"
+    }`
+  );
+
+  return tree;
+}
+
+function createAngularJsonForSubsequentRun(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    ANGULAR_JSON,
+    `{
+      "$schema": "./node_modules/@angular/cli/lib/config/schema.json",
+      "projects": {
+        "froko-app": {
+          "schematics": {
+            "@froko/ng-essentials": {
+              "jest": true,
+              "cypress": true,
+              "testcafe": true
+            }
+          },
+          "architect": {
+            "test": {
+              "builder": "@angular-devkit/build-angular:dev-server"
+            }
+          }
+        },
+        "froko-app-e2e": {}
+      },
+      "defaultProject": "froko-app"
+    }`
+  );
+
+  return tree;
+}
+
+function createPackageJson(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    PACKAGE_JSON,
+    `{
+      "scripts": {
+        "ng": "ng",
+        "start": "ng serve",
+        "build": "ng build",
+        "test": "ng test",
+        "lint": "ng lint",
+        "e2e": "ng e2e"
+      },
+      "dependencies": {
+        "@angular/animations": "^6.1.0",
+        "@angular/common": "^6.1.0",
+        "@angular/compiler": "^6.1.0",
+        "@angular/core": "^6.1.0",
+        "@angular/forms": "^6.1.0",
+        "@angular/http": "^6.1.0",
+        "@angular/platform-browser": "^6.1.0",
+        "@angular/platform-browser-dynamic": "^6.1.0",
+        "@angular/router": "^6.1.0",
+        "core-js": "^2.5.4",
+        "rxjs": "~6.2.0",
+        "zone.js": "~0.8.26"
+      },
+      "devDependencies": {
+        "@angular-devkit/build-angular": "~0.8.0",
+        "@angular/cli": "~6.2.2",
+        "@angular/compiler-cli": "^6.1.0",
+        "@angular/language-service": "^6.1.0",
+        "@types/jasmine": "~2.8.8",
+        "@types/jasminewd2": "~2.0.3",
+        "@types/node": "~8.9.4",
+        "codelyzer": "~4.3.0",
+        "jasmine-core": "~2.99.1",
+        "jasmine-spec-reporter": "~4.2.1",
+        "karma": "~3.0.0",
+        "karma-chrome-launcher": "~2.2.0",
+        "karma-coverage-istanbul-reporter": "~2.0.1",
+        "karma-jasmine": "~1.1.2",
+        "karma-jasmine-html-reporter": "^0.2.2",
+        "protractor": "~5.4.0",
+        "ts-node": "~7.0.0",
+        "tslint": "~5.11.0",
+        "typescript": "~2.9.2"
+      }
+    }`
+  );
+
+  return tree;
+}
+
+function createGlobalTsLintJson(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    TSLINT_JSON,
+    `{
+      "rules": {
+        "eofline": true,
+        "whitespace": [
+          true,
+          "check-branch",
+          "check-type"
+        ]
+      }
+    }`
+  );
+
+  return tree;
+}
 
 function createDevelopmentEnvironmentFile(tree: UnitTestTree): UnitTestTree {
   tree.create(
@@ -48,9 +449,66 @@ function createProductionEnvironmentFile(tree: UnitTestTree): UnitTestTree {
     './src/environments/environment.prod.ts',
     `
       export const environment = {
-        production: false
+        production: true
       };
     `
+  );
+
+  return tree;
+}
+
+function createKarmaConfig(tree: UnitTestTree): UnitTestTree {
+  tree.create('src/karma.conf.js', 'module.exports = function(confg) {};');
+
+  return tree;
+}
+
+function createTestTypescriptFile(tree: UnitTestTree): UnitTestTree {
+  tree.create('src/test.ts', "import { getTestBed } from '@angular/core/testing';");
+
+  return tree;
+}
+
+function createTsConfigAppInSrcDirectory(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    TSCONFIGAPP_JSON,
+    `{
+      "extends": "../tsconfig.json",
+      "compilerOptions": {
+        "outDir": "../out-tsc/app",
+        "types": []
+      },
+      "exclude": [
+        "test.ts",
+        "**/*.spec.ts"
+      ]
+    }`
+  );
+
+  return tree;
+}
+
+function createTsConfigSpecInSrcDirectory(tree: UnitTestTree): UnitTestTree {
+  tree.create(
+    '/src/tsconfig.spec.json',
+    `{
+      "extends": "../tsconfig.json",
+      "compilerOptions": {
+        "outDir": "../out-tsc/spec",
+        "types": [
+          "jasmine",
+          "node"
+        ]
+      },
+      "files": [
+        "test.ts",
+        "polyfills.ts"
+      ],
+      "include": [
+        "**/*.spec.ts",
+        "**/*.d.ts"
+      ]
+    }`
   );
 
   return tree;
