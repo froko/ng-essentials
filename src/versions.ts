12f86d996ef2997e8857b56d355fc0550f49e863
Patrick Ineichen <github@frokonet.ch>
2018-12-04 20:51:05 +0100

    Upgrade to Angular7.1

--- src/versions.ts
+++ src/versions.ts
@@ -1,6 +1,6 @@
 export const essentials = {
   // Angular base version
-  angularVersion: '7.0.1',
+  angularVersion: '7.1.1',
 
   // core-js
   coreJsVersion: '2.5.7',
@@ -9,13 +9,13 @@ export const essentials = {
   rxjsVersion: '6.3.3',
 
   // @angular-devkit/build-angular
-  buildAngularVersion: '0.10.3',
+  buildAngularVersion: '0.11.1',
 
   // @angular/cli
-  cliVersion: '7.0.3',
+  cliVersion: '7.1.1',
 
   // @types/node
-  nodeVersion: '10.12.0',
+  nodeVersion: '10.12.12',
 
   // codelizer
   codelizerVersion: '4.5.0',
@@ -30,19 +30,19 @@ export const essentials = {
   typescriptVersion: '3.1.3',
 
   // husky
-  huskyVersion: '1.1.2',
+  huskyVersion: '1.2.0',
 
   // npm-run-all
-  npmRunAllVersion: '4.1.3',
+  npmRunAllVersion: '4.1.5',
 
   // prettier
-  prettierVersion: '1.14.3',
+  prettierVersion: '1.15.3',
 
   // pretty-quick
   prettyQuickVersion: '1.8.0',
 
   // tslint-config-prettier
-  tsLintConfigPrettierVersion: '1.15.0'
+  tsLintConfigPrettierVersion: '1.17.0'
 };
 
 export const karma = {
@@ -67,10 +67,10 @@ export const karma = {
 
 export const jest = {
   // @angular-builders/jest
-  jestBuilderVersion: '1.2.5',
+  jestBuilderVersion: '7.1.1',
 
   // @types/jest
-  jestTypeVersion: '23.3.5',
+  jestTypeVersion: '23.3.10',
 
   // babel-core
   babelCoreVersion: '6.26.3',
@@ -87,29 +87,29 @@ export const jest = {
 
 export const cypress = {
   // cypress
-  cypressVersion: '3.1.0'
+  cypressVersion: '3.1.3'
 };
 
 export const testcafe = {
   // testcafe
-  testcafeVersion: '0.23.0',
+  testcafeVersion: '0.23.2',
 
   // testcafe-angular-selectors
   angularSelectorsVersion: '0.3.0',
 
   // testcafe-live
-  liveVersion: '0.1.3'
+  liveVersion: '0.1.4'
 };
 
 export const library = {
   // @angular-devkit/build-ng-packagr
-  buildNgPackagrVersion: '0.8.2',
+  buildNgPackagrVersion: '0.11.0',
 
   // ng-packagr
-  ngPackagrVersion: '4.1.1',
+  ngPackagrVersion: '4.2.1',
 
   // tsickle
-  tsickleVersion: '0.32.1',
+  tsickleVersion: '0.34.0',
 
   // tslib
   tslibVersion: '1.9.3'
