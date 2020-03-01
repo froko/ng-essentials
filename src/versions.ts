790a9151822be0ba08d10f790e0f4ca72060891a
Patrick Ineichen <github@frokonet.ch>
2020-03-01 16:05:45 +0100

    Update npm package versions

--- src/versions.ts
+++ src/versions.ts
@@ -1,42 +1,42 @@
 export const essentials = {
   // Angular base version
-  angularVersion: '8.2.14',
+  angularVersion: '9.0.4',
 
   // rxjs
   rxjsVersion: '6.5.4',
 
   // tslib
-  tslibVersion: '1.10.0',
+  tslibVersion: '1.11.1',
 
   // zone.js
-  zoneVersion: '0.9.1',
+  zoneVersion: '0.10.2',
 
   // @angular-devkit/build-angular
-  buildAngularVersion: '0.803.21',
+  buildAngularVersion: '0.900.4',
 
   // @angular/cli
   cliVersion: '8.3.21',
 
   // @types/node
-  nodeVersion: '11.13.0',
+  nodeVersion: '12.12.6',
 
   // codelizer
   codelizerVersion: '5.2.1',
 
   // ts-node
-  tsNodeVersion: '8.5.4',
+  tsNodeVersion: '8.6.2',
 
   // tslint
-  tsLintVersion: '5.20.1',
+  tsLintVersion: '6.0.0',
 
   // tslint-angular
   tsLintAngularRulesVersion: '3.0.2',
 
   // typescript
-  typescriptVersion: '3.5.3',
+  typescriptVersion: '3.7.5',
 
   // husky
-  huskyVersion: '4.0.0',
+  huskyVersion: '4.2.3',
 
   // npm-run-all
   npmRunAllVersion: '4.1.5',
@@ -53,7 +53,7 @@ export const essentials = {
 
 export const karma = {
   // @types/jasmine
-  jasmineTypeVersion: '3.5.0',
+  jasmineTypeVersion: '3.5.7',
 
   // jasmine-core
   jasmineCoreVersion: '3.5.0',
@@ -68,27 +68,27 @@ export const karma = {
   coverageReporterVersion: '2.1.1',
 
   // karma-jasmine
-  karmaJasmineVersion: '3.0.1',
+  karmaJasmineVersion: '3.1.1',
 
   // karma-jasmine-html-reporter
-  htmlReporterVersion: '1.5.1' // was 1.4.2
+  htmlReporterVersion: '1.5.2' // was 1.4.2
 };
 
 export const jest = {
   // @angular-builders/jest
-  jestBuilderVersion: '8.3.2',
+  jestBuilderVersion: '9.0.0',
 
   // @types/jest
-  jestTypeVersion: '24.0.25',
+  jestTypeVersion: '25.1.3',
 
   // babel-core
   babelCoreVersion: '6.26.3',
 
   // babel-jest
-  babelJestVersion: '24.9.0',
+  babelJestVersion: '25.1.0',
 
   // jest
-  jestVersion: '24.9.0',
+  jestVersion: '25.1.0',
 
   // jest-cli
   jestCliVersion: '24.8.0',
@@ -105,7 +105,7 @@ export const cypress = {
   preprocessorVersion: '4.0.3',
 
   // cypress
-  cypressVersion: '3.8.1',
+  cypressVersion: '4.1.0',
 
   // ts-loader
   tsLoaderVersion: '5.3.3'
@@ -113,30 +113,19 @@ export const cypress = {
 
 export const testcafe = {
   // testcafe
-  testcafeVersion: '1.7.1',
+  testcafeVersion: '1.8.2',
 
   // testcafe-angular-selectors
   angularSelectorsVersion: '0.3.2'
 };
 
-export const wallaby = {
-  // angular2-template-loader
-  angularTemplateLoader: '0.6.2',
-
-  // wallaby-webpack
-  wallabyWebpack: '3.9.15',
-
-  // ngx-wallaby-jest
-  wallabyJest: '0.0.2'
-};
-
 export const library = {
   // @angular-devkit/build-ng-packagr
-  buildNgPackagrVersion: '0.803.21',
+  buildNgPackagrVersion: '0.900.4',
 
   // ng-packagr
-  ngPackagrVersion: '5.7.1',
+  ngPackagrVersion: '9.0.2',
 
   // tsickle
-  tsickleVersion: '0.37.1'
+  tsickleVersion: '0.38.0'
 };
