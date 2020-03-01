d6f03ef2275aaf595793a499cfb118fd914d917e
Patrick Ineichen <github@frokonet.ch>
2020-03-01 16:06:02 +0100

    Update sandbox to v9

--- sandbox/README.md
+++ sandbox/README.md
@@ -1,6 +1,6 @@
-# Template
+# Sandbox
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.4.
 
 ## Development server
 
--- sandbox/src/app/app.component.spec.ts
+++ sandbox/src/app/app.component.spec.ts
@@ -5,27 +5,31 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'template'`, () => {
+  it(`should have as title 'sandbox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('template');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('sandbox');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to template!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('sandbox app is running!');
   });
 });
