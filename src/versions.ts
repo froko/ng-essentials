540255e2d005dee94a10e876adcd3c14c9b2663c
Patrick Ineichen <github@frokonet.ch>
2020-08-05 13:02:44 +0200

    Update npm package versions

--- src/versions.ts
+++ src/versions.ts
@@ -1,18 +1,18 @@
 export const essentials = {
   // Angular base version
-  angularVersion: '9.1.9',
+  angularVersion: '10.0.8',
 
   // @angular/cli
-  angularCliVersion: '9.1.7',
+  angularCliVersion: '10.0.5',
 
   // @angular-devkit/*
-  angularDevKitVersion: '0.901.7',
+  angularDevKitVersion: '0.1000.5',
 
   // rxjs
-  rxjsVersion: '6.5.5',
+  rxjsVersion: '6.6.2',
 
   // tslib
-  tslibVersion: '1.11.1',
+  tslibVersion: '2.0.0',
 
   // zone.js
   zoneVersion: '0.10.3',
@@ -21,19 +21,19 @@ export const essentials = {
   nodeVersion: '12.12.6',
 
   // codelizer
-  codelizerVersion: '5.2.2',
+  codelizerVersion: '6.0.0',
 
   // ts-node
-  tsNodeVersion: '8.10.1',
+  tsNodeVersion: '8.10.2',
 
   // tslint
-  tsLintVersion: '5.20.0',
+  tsLintVersion: '6.1.3',
 
   // tslint-angular
   tsLintAngularRulesVersion: '3.0.2',
 
   // typescript
-  typescriptVersion: '3.8.3',
+  typescriptVersion: '3.9.7',
 
   // husky
   huskyVersion: '4.2.5',
@@ -53,25 +53,25 @@ export const essentials = {
 
 export const karma = {
   // @types/jasmine
-  jasmineTypeVersion: '3.5.10',
+  jasmineTypeVersion: '3.5.12',
 
   // jasmine-core
-  jasmineCoreVersion: '3.5.0',
+  jasmineCoreVersion: '3.6.0',
 
   // jasmine-spec-reporter
   jasmineSpecReporterVersion: '5.0.2',
 
   // karma
-  karmaVersion: '5.0.9',
+  karmaVersion: '5.1.1',
 
   // karma-chrome-launcher
   chromeLauncherVersion: '3.1.0',
 
   // karma-coverage-istanbul-reporter
-  coverageReporterVersion: '3.0.2',
+  coverageReporterVersion: '3.0.3',
 
   // karma-jasmine
-  karmaJasmineVersion: '3.1.1',
+  karmaJasmineVersion: '3.3.1',
 
   // karma-jasmine-html-reporter
   htmlReporterVersion: '1.5.4'
@@ -79,10 +79,10 @@ export const karma = {
 
 export const jest = {
   // @angular-builders/jest
-  jestBuilderVersion: '9.0.1',
+  jestBuilderVersion: '10.0.0',
 
   // @types/jest
-  jestTypeVersion: '25.2.3',
+  jestTypeVersion: '26.0.8',
 
   // babel-core
   babelCoreVersion: '6.26.3',
@@ -91,21 +91,21 @@ export const jest = {
   babelJestVersion: '26.0.1',
 
   // jest
-  jestVersion: '26.0.1',
+  jestVersion: '26.2.2',
 
   // jest-preset-angular
-  presetAngularVersion: '8.2.0',
+  presetAngularVersion: '8.2.1',
 
   // ts-jest
-  tsJestVersion: '26.0.0'
+  tsJestVersion: '26.1.4'
 };
 
 export const cypress = {
   // @cypress/webpack-preprocessor
-  preprocessorVersion: '5.4.1',
+  preprocessorVersion: '5.4.2',
 
   // cypress
-  cypressVersion: '4.6.0',
+  cypressVersion: '4.12.0',
 
   // ts-loader
   tsLoaderVersion: '5.3.3'
@@ -113,10 +113,10 @@ export const cypress = {
 
 export const library = {
   // ng-packagr
-  ngPackagrVersion: '9.1.5',
+  ngPackagrVersion: '10.0.3',
 
   // tsickle
-  tsickleVersion: '0.38.1'
+  tsickleVersion: '0.39.1'
 };
 
 export const resolutions = {
